64751d1d1c141e92d15302ee65aa2cadaf4a7cd9
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-10-12 15:14:45 +0600

    Added Statistic Page

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { QuizContext } from './Root';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
 
 const Statistics = () => {
+    const quizTopics = useContext(QuizContext);
+
     return (
         <div>
-            <h2>Statistics</h2>
+            {/* {
+                quizTopics.data.map(quizTopic => <StatisticChart
+                key={quizTopic.id}
+                quizTopic={quizTopic}
+                ></StatisticChart>)
+            } */}
+            <LineChart 
+            width={1200}
+            height={500}
+            data={quizTopics.data} 
+            >
+                <Line type="monotone" dataKey="marks" stroke="#82ca9d" />
+                <XAxis dataKey='6' />
+                <YAxis />
+                <Tooltip />
+                <CartesianGrid strokeDasharray="3 3" />
+                
+            </LineChart>
         </div>
     );
 };
