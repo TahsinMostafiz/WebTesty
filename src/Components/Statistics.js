612cc394dd20d611b984a1da5ffc29c00d659d08
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-10-12 17:28:34 +0600

    fixed some bugs

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,29 +1,26 @@
-import React, { useContext } from 'react';
-import { QuizContext } from './Root';
+import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
 
 const Statistics = () => {
-    const quizTopics = useContext(QuizContext);
+    
+    
+    // const data = quizTopics.data;
+    const data = [{"id":1,"name":"React","logo":"https://live.staticflickr.com/65535/52413593240_e00326e727_o.png","total":8},{"id":2,"name":"JavaScript","logo":"https://live.staticflickr.com/65535/52412638962_12e932256a_o.png","total":9},{"id":4,"name":"CSS","logo":"https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png","total":8},{"id":5,"name":"Git","logo":"https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png","total":11}]
+    
 
     return (
-        <div>
-            {/* {
-                quizTopics.data.map(quizTopic => <StatisticChart
-                key={quizTopic.id}
-                quizTopic={quizTopic}
-                ></StatisticChart>)
-            } */}
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+           
             <LineChart 
             width={1200}
             height={500}
-            data={quizTopics.data} 
+            data={data} 
             >
-                <Line type="monotone" dataKey="marks" stroke="#82ca9d" />
-                <XAxis dataKey='6' />
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                <XAxis dataKey='name' />
                 <YAxis />
                 <Tooltip />
                 <CartesianGrid strokeDasharray="3 3" />
-                
             </LineChart>
         </div>
     );
