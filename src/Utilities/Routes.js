12a6355d43bd2609784cef7a93d605466089dffa
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-10-12 09:46:30 +0600

    Made ErrorPage

--- src/Utilities/Routes.js
+++ src/Utilities/Routes.js
@@ -1,11 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Components/Root";
+import ErrorPage from "../Components/ErrorPage";
+import Home from "../Components/Home";
+import Statistics from "../Components/Statistics";
+import Blog from "../Components/Blog";
+import About from "../Components/About";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element : <Root></Root>
+        element : <Root></Root>,
+        errorElement : <ErrorPage></ErrorPage>,
+        children : [
+            {
+                path : '/',
+                element : <Home></Home>
+            },
+            {
+                path : '/home',
+                element : <Home></Home>
+            },
+            {
+                path : '/statistics',
+                element : <Statistics></Statistics>
+            },
+            {
+                path : '/blog',
+                element : <Blog></Blog>
+            },
+            {
+                path : '/about',
+                element : <About></About>
+            },
+        ]
     }
 ]);
 
