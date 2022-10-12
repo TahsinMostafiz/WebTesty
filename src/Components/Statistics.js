b662874641ea41f47f92b79a9f6b8ec455085d20
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-10-13 01:02:37 +0600

    Statistics page bugs solved

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+import { QuizContext } from './Root';
 
 const Statistics = () => {
-    
-    
-    const data = [{"id":1,"name":"React","logo":"https://live.staticflickr.com/65535/52413593240_e00326e727_o.png","total":8},{"id":2,"name":"JavaScript","logo":"https://live.staticflickr.com/65535/52412638962_12e932256a_o.png","total":9},{"id":4,"name":"CSS","logo":"https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png","total":8},{"id":5,"name":"Git","logo":"https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png","total":11}]
-    
+    const quizTopics = useContext(QuizContext);
+    const data = quizTopics.data;
 
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
            
             <LineChart 
-            width={1200}
-            height={500}
+            width={600}
+            height={400}
             data={data} 
             >
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
